4a316f6676613e33bcc905ae8e19da34aafcb638
GuthrieW <wesleyeguthrie@gmail.com>
2020-09-01 19:30:29 -0500

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -21,6 +21,7 @@ Router.get('/cardAmounts', async (request, response) => {
 
 			for (const cardId of users[i].owned_cards) {
 				const card = await Card.findById(cardId);
+				if (!card) {
 					continue;
 				}
 				newUser._doc[`${card.player_team}`] += 1;
