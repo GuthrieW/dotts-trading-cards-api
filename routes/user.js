854047530daddfa022136f219e8e09ca66bc8f76
GuthrieW <wesleyeguthrie@gmail.com>
2021-01-25 21:08:27 -0600

    Printing first user

--- routes/user.js
+++ routes/user.js
@@ -31,7 +31,7 @@ Router.get('/printUsers', async (request, response) => {
 				isSubmitter: user.is_submitter || false,
 			})
 
-			response.status(HttpStatusCodes.OK).json({ firstUser: DottsAccount });
+			response.status(HttpStatusCodes.OK).json({ firstUser: user });
 			await DottsAccount.save();
 		}
 
